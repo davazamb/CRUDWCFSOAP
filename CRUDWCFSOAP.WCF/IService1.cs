bfcb03dfdd5a3e3d8b380de2854c313082a48d93
David Ali Zambrano Burgos <davazamb10@outlook.com>
2017-11-26 13:03:34 -0300

    Implementada los objetos para la clase en la interfase del servicio

--- CRUDWCFSOAP.WCF/IService1.cs
+++ CRUDWCFSOAP.WCF/IService1.cs
@@ -8,8 +8,28 @@ using System.Text;
 
 namespace CRUDWCFSOAP.WCF
 {
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        IEnumerable<Person> GetPerson();
+        [OperationContract]
+        void InsertPerson(Person pobj);
+        [OperationContract]
+        void Updateperson(Person pobj);
+        [OperationContract]
+        void Deleteperson(int id);
 
-
-
+    }
+    [DataContract]
+    public class Person
+    {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Address { get; set; }
+    }
 
 }
--- CRUDWCFSOAP.WCF/Service1.svc.cs
+++ CRUDWCFSOAP.WCF/Service1.svc.cs
@@ -10,6 +10,24 @@ namespace CRUDWCFSOAP.WCF
 {
     public class Service1 : IService1
     {
-      
+        public void Deleteperson(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> GetPerson()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertPerson(Person pobj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Updateperson(Person pobj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
